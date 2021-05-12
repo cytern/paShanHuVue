c3ce1fb3367e9a9eb2145ec6b3e9b6c62f37cd06
cytern <2477185748@qq.com>
2021-05-12 22:08:04 +0800

    feature: 发布前修改参数列表功能

--- d2-admin-start-kit-master/src/views/system/index/page.vue
+++ d2-admin-start-kit-master/src/views/system/index/page.vue
@@ -14,6 +14,57 @@
            投诉管理
          </el-button>
        </template>
+<!--    首先是三个大标签 分别是脚本数量  数据数量  自身等级 -->
+    <el-row :gutter="30">
+<!--      脚本数量 -->
+      <el-col :span="8">
+        <el-card style="width: 100%" class="box-card">
+         脚本市场
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card style="width: 100%" class="box-card">
+          数据市场
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card style="width: 100%" class="box-card">
+          签到
+        </el-card>
+      </el-col>
+    </el-row>
+
+<!--    两个栏目 一个是待执行任务和定时任务 一个是自己的执行器-->
+    <el-row :gutter="50">
+      <el-col :span="12">
+        <el-table
+          :data="tableData"
+          height="250"
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="待执行任务"
+            width="180">
+          </el-table-column>
+        </el-table>
+      </el-col>
+      <el-col :span="12">
+        <el-table
+          :data="tableData"
+          height="250"
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="我的执行器"
+            width="180">
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+
+
         <user-manage-dialog ref="uDia"></user-manage-dialog>
        <DataManageDialog ref="dDia"></DataManageDialog>
     <ComplaintManageDialog ref="avvDia"></ComplaintManageDialog>
